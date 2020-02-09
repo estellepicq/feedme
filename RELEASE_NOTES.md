0ea978ce771d0f0391907557566eef0886d17fd4
Estelle <est.picq@gmail.com>
2020-02-09 18:09:18 +0100

    feat(list): add query to fodmaps list

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,25 +1,3 @@
-Version 0.1.5 (16/11/2019)
------------------------------
-* Add "Bonne Maison" brand
-
-Version 0.1.4 (11/09/2019)
------------------------------
-* Add "Nicole Olivier" brand
-
-Version 0.1.3 (19/08/2019)
------------------------------
-* Add a map with address
-* Add a link to Instagram in intro section
-* Add "Stance" brand
-
-Version 0.1.2 (10/02/2019)
------------------------------
-* Add Google Analytics script
-
-Version 0.1.1 (04/02/2019)
------------------------------
-* Ready for deployment
-
-Version 0.1.0 (26/12/2018)
+Version 0.0.1 (09/02/2020)
 -----------------------------
 * First release
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,27 @@
 var searchInput = document.getElementById('searchInput');
+var delay = makeDelay(1000);
 
 searchInput.addEventListener('keyup', function() {
-	console.log(searchInput.value);
+	delay(function() {
+		var foodItems = getFodmaps(searchInput.value);
+		console.log(foodItems);
+	});
 });
+
+function makeDelay(ms) {
+	var timer = 0;
+	return function(callback){
+		clearTimeout (timer);
+		timer = setTimeout(callback, ms);
+	};
+};
+
+function getFodmaps(searchTerm) {
+	Aias.HTTP.GET('http://nutrimetrics.estellepicq.com/food?fodmaps=1&searchTerm=' + "\"" + searchTerm + "\"", "json")
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log("error", err);
+  });
+}

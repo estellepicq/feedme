d20a94c7942f19faa9f7cda78dabc890e3f94031
Estelle <est.picq@gmail.com>
2020-02-09 22:24:00 +0100

    feat(list): display and filter food items

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,42 @@
+var listElt = document.getElementById('list');
 var searchInput = document.getElementById('searchInput');
-var delay = makeDelay(1000);
+
+// Load & display food items
+var foodItems;
+getFooodItems();
 
 searchInput.addEventListener('keyup', function() {
-	delay(function() {
-		var foodItems = getFodmaps(searchInput.value);
-		console.log(foodItems);
+	var search = searchInput.value;
+	foodItems.forEach((foodItem, index) => {
+		var itemElt = document.getElementById('item_' + index);
+		if (foodItem.name.toLowerCase().includes(search.toLowerCase())) {
+			if (!itemElt.classList.contains('displayed')) {
+				itemElt.classList.add('displayed');
+			}
+		} else {
+			itemElt.classList.remove('displayed');
+		}
 	});
 });
 
-function makeDelay(ms) {
-	var timer = 0;
-	return function(callback){
-		clearTimeout (timer);
-		timer = setTimeout(callback, ms);
-	};
-};
-
-function getFodmaps(searchTerm) {
-	Aias.HTTP.GET('http://nutrimetrics.estellepicq.com/food?fodmaps=1&searchTerm=' + "\"" + searchTerm + "\"", "json")
+function getFooodItems() {
+	Aias.HTTP.GET('http://nutrimetrics.estellepicq.com/food?fodmaps=1', 'json')
   .then(response => {
-    console.log(response);
+		foodItems = response;
+		this.displayList(foodItems);
   })
   .catch(err => {
     console.log("error", err);
   });
+}
+
+function displayList(items) {
+	items.forEach((item, index) => {
+		var itemElt = document.createElement('div');
+		itemElt.textContent = item.name;
+		itemElt.classList.add('item', 'displayed');
+		itemElt.id = 'item_' + index;
+		listElt.appendChild(itemElt);
+	});
+	
 }

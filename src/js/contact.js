48bee19c9d0e2b8fafc23f34246eb74b27fb63d3
Estelle <est.picq@gmail.com>
2020-02-24 23:46:28 +0100

    add contact and legal sections modals

---
+++ src/js/contact.js
@@ -0,0 +1,49 @@
+// Contact
+var contactFormContainer = document.getElementById('contactFormContainer');
+var contactBtn = document.getElementById('contactBtn');
+var nameInput = document.getElementById('name');
+var emailInput = document.getElementById('email');
+var messageInput = document.getElementById('message');
+var errorMessage = document.getElementById('errorMessage');
+var successMessage = document.getElementById('successMessage');
+
+nameInput.addEventListener('keyup', function() {
+  setBtnState();
+});
+
+emailInput.addEventListener('keyup', function() {
+  setBtnState();
+});
+
+messageInput.addEventListener('keyup', function() {
+  setBtnState();
+});
+
+contactBtn.addEventListener('click', function() {
+  var contactBody = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  sendMail(contactBody);
+});
+
+function setBtnState() {
+  contactBtn.disabled = !nameInput.value || !emailInput.value || !messageInput.value;
+}
+
+function sendMail(contactBody) {
+  contactBtn.disabled = true;
+  Aias.HTTP.POST('/mail/send', 'json', contactBody)
+    .then(function(response) {
+      if (response.success) {
+        contactFormContainer.style.display = 'none';
+        successMessage.style.display = 'block';
+      } else {
+        errorMessage.style.display = 'block';
+      }
+    })
+    .catch(function(err) {
+      errorMessage.style.display = 'block';
+    });
+}

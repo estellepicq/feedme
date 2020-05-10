510b32988b5bedfa0dd3ca830f403e5b5a5ce4cf
Estelle <est.picq@gmail.com>
2020-05-10 23:16:09 +0200

    feat(recipe-details): add share options

--- app.js
+++ app.js
@@ -86,23 +86,10 @@ app.get('/recipes/:id', function (req, res) {
       res.render('recipes', { data: [], success: false });
     }
     const recipe = body && body.length ? body.find(recipe => recipe._id === id) : {};
-    res.render('recipe-details', { recipe });
+    const previousRecipe = body.length > 1 && body.indexOf(recipe) > 0 ? body[body.indexOf(recipe) - 1] : undefined;
+    const nextRecipe = body.length > 1 && body.indexOf(recipe) < body.length ? body[body.indexOf(recipe) + 1] : undefined;
+    res.render('recipe-details', { recipe, previousRecipe, nextRecipe });
   });
-  // res.render('recipe-details', {
-  //   recipe: {
-  //     id: 123,
-  //     name: 'Banana Bread',
-  //     tags: ['Petit-déjeuner', 'Banane'],
-  //     description: 'Banana bread léger parfait pour le petit-déjeuner, Banana bread léger parfait pour le petit-déjeuner, Banana bread léger parfait pour le petit-déjeuner',
-  //     thumbnail: 'https://images.unsplash.com/photo-1587121269960-d9d68bfb6f12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
-  //     items: ['2 bananes', '3 oeufs', '1 yaourt nature'],
-  //     instructions: ['écraser les bananes', 'tout mélanger', 'mettre au four'],
-  //     prepTime: 10,
-  //     totalTime: 50,
-  //     portions: 4,
-  //     difficulty: 'Facile'
-  //   }
-  // });
 });
 
 // Public output folder
